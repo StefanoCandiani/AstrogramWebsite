50a354d5c7a369f23bed7dd3f4ef2521ed1bcec3
Stefano Candiani <stefanocandiani1@MacBook-Air-di-Stefano.local>
2021-02-24 14:00:41 -0500

    WIP 2/24/2021

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import Card from './Insta Components/Card'
 import './App.css';
+import axios from 'axios'
 
-class App extends Component {
-  
-  // constructor(props){
-  //   super(props);
-  // }
+function App () {
 
-  render(){
+  const [imagesFetched, setImages] = useState([])
+
+  useEffect(() => {
+    axios.get("https://images-api.nasa.gov/search?q=Nebula")
+      .then(res => {
+        const imgList = res.data.collection.items.slice(0,20);
+
+        setImages(imgList);
+      })
+  }, []);
+
+  console.log(imagesFetched);
+
     return (
       <>
         <h1> Testing </h1>
-        <Card />
+        <ul>
+          {
+            imagesFetched.map((el) => {
+              return(
+                <li key={el.data.map((el) => { return el.nasa_id })}>
+                  <Card imageLink={el.href}/>
+                </li>
+              )
+            })
+          }
+        </ul>
+
       </>
     );    
-  }
 
 }
 
--- src/Insta Components/Card.js
+++ src/Insta Components/Card.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-function Card() {
+function Card( props, {imageLink} ) {
     const [likes, setLikes] = useState(0);
 
     function click () {
@@ -8,8 +8,8 @@ function Card() {
     }
 
     return(
-        <div className="card">             
-            <div><img src="" alt="..."></img></div>
+        <div className="card" key="0">             
+            <div><img src={imageLink} alt="..."></img></div>
             <div className="button">
                 <button className="like" onClick={click}><img src="https://pngimg.com/uploads/like/like_PNG15.png" alt="likeImg" height="20" width="20"></img></button>
                 <p>{likes} likes</p>

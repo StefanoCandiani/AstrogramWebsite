f668b0ac5aef54a029a5c74fff9b60f5c7f610f8
Stefano Candiani <stefanocandiani1@MacBook-Air-di-Stefano.local>
2021-03-16 21:23:36 -0400

    Updated code - 3/16/21

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import React, {useState, useEffect, Fragment} from 'react';
-import Card from './Insta Components/Card'
-import './App.css';
-import axios from 'axios'
+import React from 'react';
+import CardList from './InstaComponents/CardList.js'
+
 
 function App () {
 
-  const [imagesFetched, setFetchImages] = useState([]);
-
-  useEffect(() => {
-    axios.get("https://images-api.nasa.gov/search?q=Nebula")
-      .then(res => {
-        const imgList = [].concat(res.data.collection.items.slice(0,20)); 
-
-        setFetchImages(imgList);
-      })
-  }, []);
-
-
-    return (
-      <Fragment>
-        <ul>
-          {
-            imagesFetched.map((el) => {    
-              return(
-                <li key={el.data.map((el) => { return el.nasa_id })}>            
-                  
-                  <Card imageLink={el.links[0].href}/>
-                </li>
-              )
-            })
-          }
-        </ul>
-
-      </Fragment>
-    );    
-
+  return(
+    <div>
+      <CardList />    
+    </div>
+  )
+  
 }
 
 export default App;
---
+++ src/InstaComponents/CardList.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect, Fragment} from 'react';
+import Card from './Card.js'
+import axios from 'axios'
+
+const CardList = () => {
+    const [imagesFetched, setFetchImages] = useState([]);
+    const [search, setSearch] = useState("Nebula")
+
+  useEffect(() => {
+    axios.get(`https://images-api.nasa.gov/search?q=${search}`)
+      .then(res => {
+        const imgList = [].concat(res.data.collection.items.slice(0,20)); 
+
+        setFetchImages(imgList);
+      })
+  }, [search]);
+
+  const changeItems = (e) => {
+    console.log(e.target.value)
+    let newSearch = e.target.value;
+    setSearch(newSearch);
+  }
+
+    return (
+      <Fragment>
+        <div className="searchBar">
+          <input type="text" onChange={changeItems}></input>
+        </div>
+        <ul>
+
+          { 
+            imagesFetched.map((el) => {    
+              return(
+                <li key={el.data.map((el) => { return el.nasa_id })}> 
+                {
+                  el.links !== undefined ? <Card imageLink={el.links[0].href}/> : <h1>Couldn't Find a Result</h1>
+                }           
+                  
+                </li>
+              )
+            })              
+
+          }
+        </ul>
+
+      </Fragment>
+    );    
+
+}
+
+export default CardList;
